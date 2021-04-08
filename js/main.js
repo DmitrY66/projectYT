b10403da40cfa56f3facf42f98b9b2567426b4a5
Dmitri <ss808@mail.ru>
2021-04-09 01:17:40 +0500

    3 контент + слайдеры
    контент + слайдеры

---
+++ js/main.js
@@ -0,0 +1,34 @@
+const swiper1 = new Swiper('.channel-slider', {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 6,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.channel-button-next',
+    prevEl: '.channel-button-prev',
+  },
+});
+
+const swiper2 = new Swiper('.channel-slider-big', {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 3,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.channel-button-next-big',
+    prevEl: '.channel-button-prev-big',
+  },
+});
+
+const swiper3 = new Swiper('.channel-slider2', {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 6,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.channel-button-next2',
+    prevEl: '.channel-button-prev2',
+  },
+});
+
+
