e7ea1fc7c190629ba19511ddb99c7d2b7afe55e3
Dmitri <ss808@mail.ru>
2021-04-09 16:22:17 +0500

    4 адаптация
    адаптация

--- js/main.js
+++ js/main.js
@@ -1,7 +1,25 @@
 const swiper1 = new Swiper('.channel-slider', {
   // Optional parameters
   loop: true,
-  slidesPerView: 6,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints: {
+    1900: {
+      slidesPerView: 6,
+    },
+    1500: {
+      slidesPerView: 5,
+    },
+    1300: {
+      slidesPerView: 4,
+    },
+    1100: {
+      slidesPerView: 3,
+    },
+    760: {
+      slidesPerView: 2,
+    },
+  },
   // Navigation arrows
   navigation: {
     nextEl: '.channel-button-next',
@@ -12,7 +30,16 @@ const swiper1 = new Swiper('.channel-slider', {
 const swiper2 = new Swiper('.channel-slider-big', {
   // Optional parameters
   loop: true,
-  slidesPerView: 3,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints: {
+    1600: {
+      slidesPerView: 3,
+    },
+    1100: {
+      slidesPerView: 2,
+    },
+  },
   // Navigation arrows
   navigation: {
     nextEl: '.channel-button-next-big',
@@ -23,7 +50,25 @@ const swiper2 = new Swiper('.channel-slider-big', {
 const swiper3 = new Swiper('.channel-slider2', {
   // Optional parameters
   loop: true,
-  slidesPerView: 6,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints: {
+    1900: {
+      slidesPerView: 6,
+    },
+    1500: {
+      slidesPerView: 5,
+    },
+    1300: {
+      slidesPerView: 4,
+    },
+    1100: {
+      slidesPerView: 3,
+    },
+    760: {
+      slidesPerView: 2,
+    },
+  },
   // Navigation arrows
   navigation: {
     nextEl: '.channel-button-next2',
@@ -31,4 +76,14 @@ const swiper3 = new Swiper('.channel-slider2', {
   },
 });
 
+const searchBtn = document.querySelector('.mobile-search');
+const mobileSearch = document.querySelector('.input-group');
+searchBtn.addEventListener('click', () => {
+  mobileSearch.classList.toggle('is-open')
+})
 
+if (document.documentElement.scrollWidth <= 640) {
+  swiper1.destroy();
+  swiper2.destroy();
+  swiper3.destroy();
+}
